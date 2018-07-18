6af331f7013c6ac6e9156e86a02201e2121cb914
Py <py.cladiere@telecom-bretagne.eu>
2018-07-19 00:11:51 +0200

    Reorganised code + started using score + went to a matricial display.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,59 @@
+var scores = {
+    "scoreList" : [],
+    "unitScoreList" : {}
+};
+var columList = ["sword", "redTome", "lance", "blueTome", "axe", "greenTome"];
 
+function generateScoreList(numberOfBlessing, withMerge, withSuperBoon) {
+    scoreList = {};
+    for (let unit in units) {
+        let realUnit = units[unit];
+        let score = realUnit.calculateScore(numberOfBlessing, withMerge, withSuperBoon);
+        if (scores.scoreList.indexOf(score) === -1) {
+            scores.scoreList.push(score);
+            scores.unitScoreList[score] = {};
+        }
+        if (!scores.unitScoreList[score][realUnit.weaponType]) {
+            scores.unitScoreList[score][realUnit.weaponType] = [];
+        }
+        scores.unitScoreList[score][realUnit.weaponType].push(unit);
+    }
+    scores.scoreList.sort().reverse();
+}
+
+function add1x1Cell(grid, content, x, y) {
+    let cell = document.createElement("div");
+    cell.textContent =  content;
+    cell.style.gridColumn = x + " / span 1";
+    cell.style.gridRow = y + " / span 1";
+    grid.appendChild(cell);
+}
+
+function drawMatrix(grid) {
+    for (let i = 0; i < columList.length; i++) {
+        add1x1Cell(grid, columList[i], 2 + i, 1);
+    }
+    for (let i = 0; i < scores.scoreList.length; i++) {
+        let score = scores.scoreList[i];
+        let unitWithScore = false;
+        for (let j = 0; j < columList.length; j++) {
+            let weaponType = columList[j];
+            if (scores.unitScoreList[score][weaponType]) {
+                unitWithScore = true;
+                add1x1Cell(grid, scores.unitScoreList[score][weaponType].join(", "), j + 2, i + 2);
+            }
+        }
+        if (unitWithScore) {
+            add1x1Cell(grid, score, 1, 2 + i);
+        }
+    }
+}
 function init() {
     let grid = document.getElementById("unitDisplay");
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
-    let gridMatrice =  { "sword" : [] ,
-    "lance": [] ,
-    "axe" : [] };
-    for (let unit in units) {
-        let realUnit = units[unit];
-        if (gridMatrice[realUnit.weaponType]) {
-            gridMatrice[realUnit.weaponType].push(unit);
-        }
-
-    }
-    var currentColumn = 1;
-    for (let weaponType in gridMatrice) {
-        let weaponUnits = gridMatrice[weaponType];
-        let weaponSpan = document.createElement("div");
-        weaponSpan.style.gridColumn = currentColumn + " / span 1";
-        weaponSpan.style.gridRow = "1 / 2";
-        weaponSpan.textContent =  weaponType;
-        let span = document.createElement("div");
-        span.textContent =  weaponUnits.join(", ");
-        span.style.gridColumn = currentColumn + " / span 1";
-        span.style.gridRow = "2 / 3";
-        grid.appendChild(weaponSpan);
-        grid.appendChild(span);
-        currentColumn++;
-    }
+    generateScoreList(0, true, true);
+    drawMatrix(grid);
 }
 document.addEventListener("DOMContentLoaded", init);
